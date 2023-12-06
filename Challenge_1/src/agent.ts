648db48f529a2ca13b7fb4e01403e1af2d8ac7c0
Kevin Cai <kevinlcai@outlook.com>
2023-12-06 15:38:05 +0000

    Filter out transactions using createAgent function filter

--- Challenge_1/src/agent.ts
+++ Challenge_1/src/agent.ts
@@ -1,50 +1,43 @@
 import {
-  BlockEvent,
   Finding,
-  Initialize,
-  HandleBlock,
-  HealthCheck,
   HandleTransaction,
-  HandleAlert,
-  AlertEvent,
-  TransactionEvent,
   FindingSeverity,
   FindingType,
+  TransactionEvent,
 } from "forta-agent";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
+export const CREATE_AGENT_EVENT_ABI =
+  "function createAgent(uint256 agentId, address, string metadata, uint256[] chainIds) external";
 export const FORTA_BOTS_ADDRESS = "0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8";
-export const TETHER_DECIMALS = 10;
-let findingsCount = 0;
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+function provideHandleTransaction(
+  eventABI: string,
+  fortaBotsAddress: string,
+): HandleTransaction {
+  return async function handleTransaction(txEvent: TransactionEvent) {
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+    const findings: Finding[] = [];
 
-  // filter for Forta Bot transactions
-  // const fortaDeploymentEvents = txEvent.filterLog(
-  //   ERC20_TRANSFER_EVENT,
-  //   FORTA_BOTS_ADDRESS
-  // );
+  // Filter Forta Bot transactions
+  const fortaDeploymentEvents = txEvent.filterFunction(
+    eventABI,
+    fortaBotsAddress
+  );
 
-  // hard code events for now
-  const fortaDeploymentEvents = [txEvent, txEvent];
+  // const fortaDeploymentEvents: TransactionEvent[] = [txEvent];
 
-  fortaDeploymentEvents.forEach((transferEvent) => {
-    // extract address of transaction
-    const address = transferEvent.from;
+  console.log("LOGS")
+  console.log(fortaDeploymentEvents)
 
-    // if more than 10,000 Tether were transferred, report it
-    if (txEvent.from == FORTA_BOTS_ADDRESS) {
+  for (const transferEvent of fortaDeploymentEvents) {
+    const address = txEvent.from
+
+    // Compare with transferEvent.from instead of txEvent.from
+    if (address === fortaBotsAddress) {
       findings.push(
         Finding.fromObject({
           name: "Forta TX",
-          description: `Forta contract tx`,
+          description: "Forta contract tx",
           alertId: "FORTA-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
@@ -53,18 +46,15 @@ const handleTransaction: HandleTransaction = async (
           },
         })
       );
-      findingsCount++;
     }
-  });
+  }
 
   return findings;
-};
+  }
+}
+
 
 
 export default {
-  // initialize,
-  handleTransaction,
-  // healthCheck,
-  // handleBlock,
-  // handleAlert
+  handleTransaction: provideHandleTransaction(CREATE_AGENT_EVENT_ABI, FORTA_BOTS_ADDRESS),
 };
