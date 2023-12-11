8d4c501cc3811fcf29eefa5ac88d2c074e687586
Kevin Cai <kevinlcai@outlook.com>
2023-12-11 15:17:50 +0000

    Add test case for multiple different function calls

--- Challenge_1/src/agent.spec.ts
+++ Challenge_1/src/agent.spec.ts
@@ -9,6 +9,7 @@ import {
 } from "forta-agent";
 import agent, {
   CREATE_AGENT_EVENT_ABI,
+  UPDATE_AGENT_EVENT_ABI,
   FORTA_BOTS_ADDRESS,
   REGISTRY_ADDRESS,
 } from "./agent";
@@ -24,12 +25,18 @@ describe("Forta Bot Deployment Agent", () => {
   });
 
   const mockTxEventData1 = [1, createAddress("0x01"), "Mock tx 1", [137]];
-
   const mockTxEventData2 = [1, createAddress("0x02"), "Mock tx 2", [137]];
 
+  const WRONG_FUNCTION_ABI =
+    "function wrongFunction(uint256 agentId,address ,string metadata,uint256[] chainIds)";
+
   describe("handleTransaction", () => {
+    const createAgentInterface = new Interface([CREATE_AGENT_EVENT_ABI]);
+    const updateAgentInterface = new Interface([UPDATE_AGENT_EVENT_ABI]);
+    const wrongFunctionInterface = new Interface([WRONG_FUNCTION_ABI]);
+    const wrongAddress = createAddress("0x00");
+
     it("returns finding if there is one bot deployment", async () => {
-      const createAgentInterface = new Interface([CREATE_AGENT_EVENT_ABI]);
       let mockTxEvent: TransactionEvent = new TestTransactionEvent()
         .setTo(REGISTRY_ADDRESS)
         .setFrom(FORTA_BOTS_ADDRESS)
@@ -44,56 +51,172 @@ describe("Forta Bot Deployment Agent", () => {
 
       expect(findings).toStrictEqual([
         expect.objectContaining({
-          name: 'Forta TX',
-          description: 'Forta contract tx',
-          alertId: 'FORTA-1',
-          protocol: 'ethereum',
-          severity: 2,
-          type: 4,
+          name: "Forta TX",
+          description: "Forta contract tx",
+          alertId: "FORTA-1",
+          protocol: "ethereum",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
           metadata: {
-            createAgentEventAddress: '0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8'
+            createAgentEventAddress: FORTA_BOTS_ADDRESS,
           },
           addresses: [],
           labels: [],
-          uniqueKey: '',
+          uniqueKey: "",
           source: {},
         }),
       ]);
-
-
     });
 
-    // it("returns findings if there are multipe bot deployments", async () => {
-    //   let mockTxEvent: TransactionEvent = new TestTransactionEvent();
-    //   const findings = await handleTransaction(mockTxEvent);
-    //   expect(findings).toStrictEqual([]);
-    // });
+    it("returns findings if there are multiple bot deployments", async () => {
+      let mockTxEvent: TransactionEvent = new TestTransactionEvent()
+        .setTo(REGISTRY_ADDRESS)
+        .setFrom(FORTA_BOTS_ADDRESS)
+        .addTraces({
+          function: createAgentInterface.getFunction("createAgent"),
+          to: REGISTRY_ADDRESS,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData1,
+        })
+        .addTraces({
+          function: createAgentInterface.getFunction("createAgent"),
+          to: REGISTRY_ADDRESS,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData1,
+        });
 
-    // it("returns empty findings if the deployment address is wrong", async () => {
-    //   let mockTxEvent: TransactionEvent = new TestTransactionEvent()
-    //     .setFrom(FORTA_BOTS_ADDRESS)
-    //     .setTo(REGISTRY_ADDRESS)
-    //     .addTraces();
-    //   const findings = await handleTransaction(mockTxEvent);
-    //   expect(findings).toStrictEqual([]);
-    // });
+      const findings = await handleTransaction(mockTxEvent);
 
-    // it("returns empty findings if the registry address is wrong", async () => {
-    //   let mockTxEvent: TransactionEvent = new TestTransactionEvent()
-    //     .setFrom(FORTA_BOTS_ADDRESS)
-    //     .setTo(REGISTRY_ADDRESS)
-    //     .addTraces();
-    //   const findings = await handleTransaction(mockTxEvent);
-    //   expect(findings).toStrictEqual([]);
-    // });
+      expect(findings).toStrictEqual([
+        expect.objectContaining({
+          name: "Forta TX",
+          description: "Forta contract tx",
+          alertId: "FORTA-1",
+          protocol: "ethereum",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            createAgentEventAddress: FORTA_BOTS_ADDRESS,
+          },
+          addresses: [],
+          labels: [],
+          uniqueKey: "",
+          source: {},
+        }),
+        expect.objectContaining({
+          name: "Forta TX",
+          description: "Forta contract tx",
+          alertId: "FORTA-1",
+          protocol: "ethereum",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            createAgentEventAddress: FORTA_BOTS_ADDRESS,
+          },
+          addresses: [],
+          labels: [],
+          uniqueKey: "",
+          source: {},
+        }),
+      ]);
+    });
 
-    // it("returns empty findings if the function ABI is wrong", async () => {
-    //   let mockTxEvent: TransactionEvent = new TestTransactionEvent()
-    //     .setFrom(FORTA_BOTS_ADDRESS)
-    //     .setTo(REGISTRY_ADDRESS)
-    //     .addTraces();
-    //   const findings = await handleTransaction(mockTxEvent);
-    //   expect(findings).toStrictEqual([]);
-    // });
+    it("returns multiple findings for different function calls", async () => {
+      let mockTxEvent: TransactionEvent = new TestTransactionEvent()
+        .setTo(REGISTRY_ADDRESS)
+        .setFrom(FORTA_BOTS_ADDRESS)
+        .addTraces({
+          function: updateAgentInterface.getFunction("updateAgent"),
+          to: REGISTRY_ADDRESS,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData2,
+        })
+        .addTraces({
+          function: createAgentInterface.getFunction("createAgent"),
+          to: REGISTRY_ADDRESS,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData1,
+        });
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([
+        expect.objectContaining({
+          name: "Forta TX",
+          description: "Forta contract tx",
+          alertId: "FORTA-1",
+          protocol: "ethereum",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            createAgentEventAddress: FORTA_BOTS_ADDRESS,
+          },
+          addresses: [],
+          labels: [],
+          uniqueKey: "",
+          source: {},
+        }),
+        expect.objectContaining({
+          name: "Forta TX",
+          description: "Forta contract tx",
+          alertId: "FORTA-1",
+          protocol: "ethereum",
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {
+            updateAgentEventAddress: FORTA_BOTS_ADDRESS,
+          },
+          addresses: [],
+          labels: [],
+          uniqueKey: "",
+          source: {},
+        }),
+      ]);
+    });
+
+    it("returns empty findings if the deployment address is wrong", async () => {
+      let mockTxEvent: TransactionEvent = new TestTransactionEvent()
+        .setTo(REGISTRY_ADDRESS)
+        .setFrom(wrongAddress)
+        .addTraces({
+          function: createAgentInterface.getFunction("createAgent"),
+          to: REGISTRY_ADDRESS,
+          from: wrongAddress,
+          arguments: mockTxEventData1,
+        });
+
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if the registry address is wrong", async () => {
+      let mockTxEvent: TransactionEvent = new TestTransactionEvent()
+        .setTo(wrongAddress)
+        .setFrom(FORTA_BOTS_ADDRESS)
+        .addTraces({
+          function: createAgentInterface.getFunction("createAgent"),
+          to: wrongAddress,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData1,
+        });
+
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if the function ABI is wrong", async () => {
+      let mockTxEvent: TransactionEvent = new TestTransactionEvent()
+        .setTo(REGISTRY_ADDRESS)
+        .setFrom(FORTA_BOTS_ADDRESS)
+        .addTraces({
+          function: wrongFunctionInterface.getFunction("wrongFunction"),
+          to: REGISTRY_ADDRESS,
+          from: FORTA_BOTS_ADDRESS,
+          arguments: mockTxEventData1,
+        });
+
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+    });
   });
 });
--- Challenge_1/src/agent.ts
+++ Challenge_1/src/agent.ts
@@ -7,7 +7,7 @@ import {
 } from "forta-agent";
 
 export const CREATE_AGENT_EVENT_ABI = "function createAgent(uint256 agentId,address ,string metadata,uint256[] chainIds)";
-export const UPDATE_AGENT_EVENT_ABI = "function updateAgent(uint256 agentId,string metadata,uint256[] chainIds) external";
+export const UPDATE_AGENT_EVENT_ABI = "function updateAgent(uint256 agentId,address ,string metadata,uint256[] chainIds) external";
 export const REGISTRY_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 export const FORTA_BOTS_ADDRESS = "0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8";
 
